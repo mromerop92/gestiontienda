fdadb0aca0e019f571fce37da42d0cbcb283a3b5
mromerop92 <antonioromeropereira@yahoo.es>
2024-06-11 18:00:45 +0200

    Creacion de proyecto

---
+++ src/main/java/gestiontienda/App.java
@@ -0,0 +1,16 @@
+package gestiontienda;
+
+/**
+ * @author Moises Antonio Romero Pereira
+ * @version 1.0 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " Sistemas de Gestión " );
+        System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Recuperacion de producto.\n4. Movimientos de caja.");
+
+    }
+
+}
