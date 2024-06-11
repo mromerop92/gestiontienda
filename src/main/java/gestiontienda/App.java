1f00e70f8be80ae6e1286f63dc8c32bb949f13e2
mromerop92 <antonioromeropereira@yahoo.es>
2024-06-11 20:41:23 +0200

    avances y creaciones de clases

--- src/main/java/gestiontienda/App.java
+++ src/main/java/gestiontienda/App.java
@@ -1,15 +1,25 @@
 package gestiontienda;
+import  java.util.*;
+
 
 /**
+ * Sistemas de Gestión de tienda de senguda manno.
  * @author Moises Antonio Romero Pereira
- * @version 1.0 
+ * @version 10/06/2024  1.0 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    
+    public static Scanner leerOpcion = new Scanner(System.in);
+    public static int opcion;
+    
+    /*
+     * Metodos
+     */
+    public static void main( String[] args ) {
+
         System.out.println( " Sistemas de Gestión " );
-        System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Recuperacion de producto.\n4. Movimientos de caja.");
+        System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Recuperación de producto.\n4. Movimientos de caja.");
+        opcion = leerOpcion.nextInt();
 
     }
 
