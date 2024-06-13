8748f684c53ce734b3c4c8483d180382badb939c
mromerop92 <antonioromeropereira@yahoo.es>
2024-06-13 14:33:09 +0200

    avances, metodos comprar, gestion de caja

--- src/main/java/gestiontienda/App.java
+++ src/main/java/gestiontienda/App.java
@@ -9,41 +9,46 @@ import  java.util.*;
 public class App {
     
     public static Scanner leerOpcion = new Scanner(System.in);
-    public static int opcion;    
+    public static int inicio;
     /*
      * Metodos
      */
     public static void main( String[] args ) {
 
         Gestion gestion = new Gestion();
+        
+        do {     
+            System.out.println("Bienvenido \n1. Continuar. \n2. Salir");
+            inicio = leerOpcion.nextInt();      
+        
+            System.out.println( " Sistemas de Gestión " );
+            System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Buscar productos. \n4. Buscar vendedores. \n5. Recuperación de producto.\n6. Movimientos de caja.");
+        
+            switch (leerOpcion.nextInt()) {
+                case 1:
+                    gestion.comprar();                
+                    break;
+                case 2:
+                    gestion.vender();
+                    break;
+                case 3:
+                    gestion.buscaProductos();
+                    break;
+                case 4:
+                    gestion.buscaVendedor();
+                    break;
+                case 5:
+                    gestion.recuperacionProducto();
+                    break;
+                case 6:
+                    gestion.movimientosCaja();
+                    break;        
+                default:
+                    System.out.println("Opción incorrecta");
+                    break;
+            }
 
-        System.out.println( " Sistemas de Gestión " );
-        System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Buscar productos. \n4. Buscar vendedores. \n5. Recuperación de producto.\n6. Movimientos de caja.");
-        opcion = leerOpcion.nextInt();
-
-        switch (opcion) {
-            case 1:
-                gestion.comprar();                
-                break;
-            case 2:
-                gestion.vender();
-                break;
-            case 3:
-                gestion.buscaProductos();
-                break;
-            case 4:
-                gestion.buscaVendedor();
-                break;
-            case 5:
-                gestion.recuperacionProducto();
-                break;
-            case 6:
-                gestion.movimientosCaja();
-                break;        
-            default:
-                System.out.println("Opción incorrecta");
-                break;
-        }
+        } while (inicio==1);
 
     }
 
--- src/main/java/gestiontienda/Gestion.java
+++ src/main/java/gestiontienda/Gestion.java
@@ -10,6 +10,17 @@ public class Gestion {
     //leer datos ingresados
     Scanner leer = new Scanner(System.in);
     Date date = new Date();
+    
+    //variable de cambio de valor por puntos de compra 10% adicional
+    BigDecimal valorPuntosCompra = BigDecimal.valueOf(1.10);
+    //variable saldo minimo en caja 1000
+    BigDecimal minimoCaja = BigDecimal.valueOf(1000);
+    // varibale caja fuerte
+    BigDecimal cajaFuerte;
+    // variable de valor a comparar para guardar a caja fuerte
+    BigDecimal aCajafuerte = BigDecimal.valueOf(3000);
+    //variable bandera de compra o venta para gestoin de caja
+    boolean compraVenta;
 
     //constructor
     public Gestion(){}
@@ -20,22 +31,22 @@ public class Gestion {
 
     //metodos
     public void comprar(){
+        compraVenta = true;
+
         Vendedor vendedor = new Vendedor();
         Productos productos = new Productos();        
-        //variable de cambio de valor por puntos de compra
-        BigDecimal valorPuntosCompra = BigDecimal.valueOf(1.10);
 
         System.out.println("Compras.");
         // detalle vendedor
-        System.out.println("Nombre del vendedor");
+        System.out.println("> Nombre del vendedor");
         vendedor.setNombreVendedor(leer.nextLine());
-        System.out.println("Identificación (DNI/NIE/CIF)");
+        System.out.println("> Identificación (DNI/NIE/CIF)");
         vendedor.setIdentificacion(leer.nextLine());
 
         // detalles producto
         System.out.println("> Nombre del producto");
         productos.setNombreProducto(leer.nextLine());
-        System.out.println("> Categoria \n1. Electrodomesticos \n2. Técnologia \n3. Ropa");
+        System.out.println("> Categoria \n1. Electrodomesticos \n2. Técnologia \n3. Otros");
         productos.setCveCategoria(leer.nextInt());
         productos.setDescCategoria((productos.getCveCategoria() == 1) ? "Electrodomestico" : "Tècnologia");
         productos.setFechaAdquisicion(date); // verificar formato
@@ -46,32 +57,49 @@ public class Gestion {
         // convertir precio a puntos de compra si|no
         if (!productos.isCompraEfectivo())
         productos.setPuntosCompra(productos.getPrecio().multiply(valorPuntosCompra));
-
-
-        
-        
-        System.out.println("********** retorno **********");
-        System.out.println("prueba de descripcoin   " + productos.getDescCategoria());
-        System.out.println(productos.getPuntosCompra());
+        System.out.println("****** productos *********");
         System.out.println(productos);
-
-        buscaProductos();
+        System.out.println("********* caja **************");
+        gestionCaja(compraVenta, productos.getPrecio());
 
     }
 
     public Productos[] buscaProductos(){        
         return productos;
     }
-
+    //45% mas en precio    
     public void vender(){}
 
+    // buscar por nombre y por identificacion
     public Vendedor[] buscaVendedor(){
         return vendedor;
     }
 
     public void recuperacionProducto(){}
+    // espera bandera de compraVenta, precio para gestion
+    public void gestionCaja(boolean prCompraOVenta, BigDecimal prPrecio){
+        if(prCompraOVenta){
+            if(minimoCaja.compareTo(prPrecio)==1){
+                minimoCaja.subtract(prPrecio);
+                System.out.println("Compra OK");
+            } else {
+                System.out.println("ADVERTENCIA \nSaldo de caja menor");
+            }
+        } else{
+            minimoCaja.add(prPrecio);
+            if(minimoCaja.compareTo(aCajafuerte)==1){
+                BigDecimal diferencia = minimoCaja.subtract(new BigDecimal(1000));
+                cajaFuerte.add(diferencia);
+                minimoCaja.subtract(diferencia);        
+                System.out.println("Se salvo en caja fuerte");
+                System.out.println("Venta OK");
+                System.out.println("*******" + minimoCaja + " " + cajaFuerte);
+            } else {
+                System.out.println("Venta OK");
+            }
+        }
 
-    public void gestionCaja(){}
+    }
 
     public void movimientosCaja(){}
     
