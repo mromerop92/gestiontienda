fa71ba6a5555c22f7275a61ac215970d0f625c0a
mromerop92 <antonioromeropereira@yahoo.es>
2024-06-13 22:52:58 +0200

    avances de metodo comprar, buscarProducto, buscarVendedor

--- src/main/java/gestiontienda/App.java
+++ src/main/java/gestiontienda/App.java
@@ -4,38 +4,66 @@
  * @version 10/06/2024  1.0 
  */
 package gestiontienda;
+import java.math.BigDecimal;
 import  java.util.*;
 
 public class App {
     
     public static Scanner leerOpcion = new Scanner(System.in);
     public static int inicio;
-    /*
-     * Metodos
-     */
-    public static void main( String[] args ) {
+    
+    /* variables de meto comprar  */
+        //leer datos ingresados
+    public static Scanner leer = new Scanner(System.in);
+    public static Date date = new Date();
 
-        Gestion gestion = new Gestion();
-        
-        do {     
-            System.out.println("Bienvenido \n1. Continuar. \n2. Salir");
-            inicio = leerOpcion.nextInt();      
-        
-            System.out.println( " Sistemas de Gestión " );
+    //variable vendedor
+    public static String nombreVende;
+    public static String numIdentificacion;
+
+    //variable detalles productos
+    public static int id = 0;
+    public static String nombrePro;
+    public static String descCategoria;
+    public static int cveCategoria;
+    public static Date fechaAdquisicio;
+    public static BigDecimal precio;    
+    public static BigDecimal puntosCompra;
+    public static boolean compraEnEfectivo;
+    public static boolean devolucion = true;
+    //variable bandera de compra o venta para gestoin de caja
+    public static boolean compraVenta;
+    //variable de cambio de valor por puntos de compra 10% adicional
+    private static BigDecimal valorPuntosCompra = BigDecimal.valueOf(1.10);
+    //catalogo productos
+    public static Productos catalogo[] = new Productos[Productos.dimesionArray];
+    //catalogo vendedor
+    public static Vendedor catalogoVende[] = new Vendedor[Vendedor.dimesionArray];
+    /* ----------------  */
+
+    public static void main( String[] args ) {  
+        Gestion gestion = new Gestion();      
+               
+        System.out.println("Gestión de tienda \n Bienvenido \n1. Continuar. \n2. Salir");
+        inicio = leerOpcion.nextInt();    
+                            
+        while (inicio == 1) { 
+            System.out.println("");
+            System.out.println( "Sistemas de Gestión" );
             System.out.println("Seleccion la operación: \n1. Comprar.\n2. Vender.\n3. Buscar productos. \n4. Buscar vendedores. \n5. Recuperación de producto.\n6. Movimientos de caja.");
         
             switch (leerOpcion.nextInt()) {
                 case 1:
-                    gestion.comprar();                
+                    comprar();                
                     break;
                 case 2:
-                    gestion.vender();
+                    gestion.vender(catalogo);
                     break;
                 case 3:
-                    gestion.buscaProductos();
+                    gestion.buscaProductos(catalogo);
                     break;
                 case 4:
-                    gestion.buscaVendedor();
+                    gestion.buscaVendedor(catalogoVende);
                     break;
                 case 5:
                     gestion.recuperacionProducto();
@@ -44,12 +72,67 @@ public class App {
                     gestion.movimientosCaja();
                     break;        
                 default:
-                    System.out.println("Opción incorrecta");
+                    System.out.println("-> Opción incorrecta. <- \n");
                     break;
             }
+            System.out.println("Otra operación \n1. Si \n2. Salir");
+            inicio = leerOpcion.nextInt();
+        }   
+    }
 
-        } while (inicio==1);
+     //metodo compra
+     public static void comprar(){
+        Gestion gestion = new Gestion();
+        compraVenta = true;   
+
+        System.out.println("\n Compra.");  
+        // detalle vendedor
+        System.out.println("> Nombre del vendedor");
+        nombreVende = leer.nextLine();
+        System.out.println("> Identificación (DNI/NIE/CIF)");
+        numIdentificacion = leer.nextLine();
+
+        // detalles producto 
+        System.out.println("> Nombre del producto");
+        nombrePro =leer.nextLine();
+        System.out.println("> Categorias \n1. Electrodomesticos \n2. Técnologia \n3. Otros");
+        cveCategoria = leer.nextInt();
+        descCategoria = ((cveCategoria == 1) ? "Electrodomestico" : "Tècnologia");
+        fechaAdquisicio = date; // verificar formato
+        System.out.println("> Precio");
+        precio = leer.nextBigDecimal();
+        System.out.println("> Compra en \n1. Efectivo \n2. Puntos de compra");
+        compraEnEfectivo = ((leer.nextInt() == 1) ? true : false);
+        // convertir precio a puntos de compra si|no
+        if (!compraEnEfectivo)
+        puntosCompra = (precio.multiply(valorPuntosCompra));
+        gestion.gestionCaja(compraVenta, precio);
+
+        //cargar producto
+        Productos pro1 = new Productos(id,nombrePro,descCategoria,cveCategoria,fechaAdquisicio,precio,puntosCompra, compraEnEfectivo, devolucion);
+        //cargar vendedor
+        Vendedor ven1 = new Vendedor(id,nombrePro,descCategoria,cveCategoria,fechaAdquisicio,precio,puntosCompra,compraEnEfectivo,devolucion,id,nombreVende,numIdentificacion);
+        
+        catalogo[((int)id)] = pro1;
+        catalogoVende[((int)id)] = ven1;
+
+        
+
+        id++;
+        limparVaribles();
 
     }
+
+    public static void limparVaribles(){                
+        inicio = 0;       
+        nombreVende = "";
+        numIdentificacion = "";
+        nombrePro = "";
+        descCategoria = "";
+        cveCategoria = 0;
+        precio = BigDecimal.valueOf(0);    
+        puntosCompra = BigDecimal.valueOf(0);
+    }
+
 
 }
--- src/main/java/gestiontienda/Vendedor.java
+++ src/main/java/gestiontienda/Vendedor.java
@@ -4,28 +4,39 @@ import java.util.*;
 
 public class Vendedor extends Productos {
     //propiedades
+    private int idProducto;
     private String nombreVendedor;
     private String identificacion;
+    public static int dimesionArray = 10;
 
     //constructor
     public Vendedor(){}
-    public Vendedor(String nombreProducto, String descCategoria, int cveCategoria, Date fechaAdquisicion, BigDecimal precio, BigDecimal puntosCompra,
-    boolean compraEfectivo, boolean compraPuntos, boolean devolucion, String nombreVendedor, String identificacion){
-        super(nombreProducto, descCategoria, cveCategoria, fechaAdquisicion, precio, puntosCompra, compraEfectivo, compraPuntos, devolucion);
+    public Vendedor(int id, String nombreProducto, String descCategoria, int cveCategoria, Date fechaAdquisicion, BigDecimal precio, BigDecimal puntosCompra,
+    boolean compraEfectivo, boolean devolucion, int idProducto, String nombreVendedor, String identificacion){
+        super(id, nombreProducto, descCategoria, cveCategoria, fechaAdquisicion, precio, puntosCompra, compraEfectivo, devolucion);
+        this.idProducto = idProducto;
         this.nombreVendedor = nombreVendedor;
-        this.identificacion = identificacion;        
+        this.identificacion = identificacion;
+        
+        dimesionArray++;
     }
 
     //metodos
     @Override
     public String toString(){
-        return "Nombre Vendedor " + this.nombreVendedor + "\n" + "Identificacion (DNI/NIE/CIF) " + this.identificacion;
+        return "id Vendedor "+ "\n" +"Nombre Vendedor " + this.nombreVendedor + "\n" + "Identificacion (DNI/NIE/CIF) " + this.identificacion;
+    }
+    public int getIdProducto() {
+        return idProducto;
     }
     public String getNombreVendedor() {
         return nombreVendedor;
     }
     public String getIdentificacion() {
         return identificacion;
+    }
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
     }
     public void setNombreVendedor(String nombreVendedor) {
         this.nombreVendedor = nombreVendedor;
